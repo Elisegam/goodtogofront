ccb6a05d723f1a1e9bed97ece360da87ed7ac4c9
Elise <elisegamassa@gmail.com>
2019-07-26 15:54:05 +0200

    ckjdslc

--- src/Components/RecapMenu.jsx
+++ src/Components/RecapMenu.jsx
@@ -1,88 +1,38 @@
-import React, { Component } from "react";
-import DisplayPrice from "../Components/DisplayPrice";
-import axios from "axios";
+import React from "react";
+import DisplayPrice from "./DisplayPrice";
+import { NavLink } from "react-router-dom";
 
-const apiHandler = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true
-});
-
-class Recap extends Component {
-  state = {
-    purchases: []
-  };
-
-  componentDidMount() {
-    apiHandler
-      .get("/api/users/last-order")
-      .then(res => {
-        console.log(res, "update please ?");
-        const purchases = res.data.purchases;
-        this.setState({
-          purchases,
-          lastOrder: purchases[purchases.length - 1]
-        });
-      })
-      .catch(err => console.log(err));
-  }
-
-  render() {
-    console.log("called first time");
-    console.log(this.state.purchases);
-    if (!this.state.lastOrder) return null;
-    return (
-      <div>
-        <div>
-          <h2
+const RecapMenu = ({ selectedItems, handleDelete }) => {
+  return (
+    <div>
+      {selectedItems.map((item, i) => (
+        <div key={i}>
+          <p
             style={{
               textAlign: "center",
-              color: "blue",
-              fontStyle: "bold"
+              fontWeight: "bold",
+              textTransform: "capitalize"
             }}
           >
-            Recapitulatif de la commande Actuelle
-          </h2>
-          {this.state.lastOrder.articles.map(article => (
-            <div style={{ fontStyle: "bold", textAlign: "center" }}>
-              <p>{article.title}</p>
-              <img src={article.image} alt={article.title} />
-            </div>
-          ))}
-          <div>
-            <h3
-              style={{ fontStyle: "bold", textAlign: "center", color: "blue" }}
-            >
-              Prix total :
-              <DisplayPrice items={this.state.lastOrder.articles} />
-            </h3>
-          </div>
-          <h3 style={{ fontStyle: "bold", textAlign: "center", color: "blue" }}>
-            Numéro de commande :{this.state.lastOrder.orderRef}
-          </h3>
+            {item.title}
+          </p>
+          <img
+            src={item.image}
+            alt="item"
+            style={{ width: "7em", margin: "auto" }}
+          />
+          <button onClick={e => handleDelete(i)}>
+            <i class="fas fa-minus-circle" />
+          </button>
+          <hr style={{ width: "80%" }} />
         </div>
-        <hr />
-        <h2
-          style={{
-            textAlign: "center",
-            color: "blue",
-            fontStyle: "bold"
-          }}
-        >
-          Historique de commandes
-        </h2>
-        {this.state.purchases.map((purchase, i) => (
-          <div>
-            <div>Achat n°{i + 1}</div>
-            {purchase.articles.map(article => (
-              <div>
-                <p>{article.title}</p>
-              </div>
-            ))}
-            <p>{purchase.orderRef}</p>
-          </div>
-        ))}
+      ))}
+      <div className="display-price">
+        <DisplayPrice items={selectedItems} />
+        <NavLink to="/payment">PAY NOW</NavLink>
       </div>
-    );
-  }
-}
-export default Recap;
+    </div>
+  );
+};
+
+export default RecapMenu;
